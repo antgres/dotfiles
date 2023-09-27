1e4354edfbd93ad223eb48b2d5a8a7f6ca40fc04
Anton Gres <anton.gres@ifm.com>
2023-09-27 14:06:34 +0200

    EoD
    Signed-off-by: Anton Gres <anton.gres@ifm.com>

--- .root/reload-stow.sh
+++ .root/reload-stow.sh
@@ -8,31 +8,31 @@ BLUE='\033[1;34m'
 RED='\033[1;30m'
 NC='\033[0m'
 
-# Navigate to the directory of this script (generally ~/.dotfiles/.bin)
+# Navigate to the directory of this script (generally ~/.dotfiles/.root)
 cd $(dirname $(readlink -f $0))
 cd ..
 
-echo -e "${BLUE}Stashing existing changes...${NC}"
+echo "${BLUE}Stashing existing changes...${NC}"
 stash_result=$(git stash push -m "sync-dotfiles: Before syncing dotfiles")
 needs_pop=1
 if [ "$stash_result" = "No local changes to save" ]; then
     needs_pop=0
 fi
 
-echo -e "${BLUE}Pulling updates from dotfiles repo...${NC}"
+echo "${BLUE}Pulling updates from dotfiles repo...${NC}"
 echo
-git pull origin master
+git pull origin devel/dalo/debian/test
 echo
 
-if [[ $needs_pop -eq 1 ]]; then
-    echo -e "${BLUE}Popping stashed changes...${NC}"
+if [ $needs_pop -eq 1 ]; then
+    echo "${BLUE}Popping stashed changes...${NC}"
     echo
-    git stash pop
+    git stash pop 1>/dev/null
 fi
 
 unmerged_files=$(git diff --name-only --diff-filter=U)
-if [[ ! -z $unmerged_files ]]; then
-   echo -e "${RED}The following files have merge conflicts after popping the stash:${NC}"
+if [ ! -z $unmerged_files ]; then
+   echo "${RED}The following files have merge conflicts after popping the stash:${NC}"
    echo
    printf %"s\n" $unmerged_files  # Ensure newlines are printed
 else
